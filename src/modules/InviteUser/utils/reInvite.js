6053f548f035158f04159a25a1c1e6f409335171
saugat977 <jungsaugat@gmail.com>
2022-08-26 10:42:02 +0545

    --amend

--- src/modules/InviteUser/utils/reInvite.js
+++
@@ -1,8 +0,0 @@
-const {getInvitationData,inviteUser} = import('../services/index')
-
-export default async function reInvite(){
-const invitationData = await getInvitationData()
-const invite = await inviteUser(invitationData)
-console.log("User Invited");
-}
-
