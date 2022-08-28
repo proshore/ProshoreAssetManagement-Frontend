a02293162263410790b417b8439d7d31c22dd383
anantKC <ananta007kc@gmail.com>
2022-08-28 11:15:03 +0545

    Some parts of Dashboard Ui is complete

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import registerRoutes from "@/modules/register/router"
 import loginRoutes from "@/modules/login/router"
 
 const ViewHome = () => import('@/views/ViewHome.vue')
+const ViewDashBoard = () => import('@/components/DashBoard.vue')
 
 const router = createRouter({
     history: createWebHistory(),
@@ -12,6 +13,11 @@ const router = createRouter({
             path:"/",
             name:"home",
             component: ViewHome
+    },
+    {
+        path:"/dashboard",
+        name:"dashboard",
+        component: ViewDashBoard
     },
     registerRoutes,
     loginRoutes
