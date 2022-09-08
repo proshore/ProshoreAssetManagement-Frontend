6e28d517daa261de7c782bb1f12b35ea27d20b3e
anantKC <ananta007kc@gmail.com>
2022-09-08 17:46:25 +0545

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
-<script >
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
-import { RouterView } from 'vue-router';
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <RouterView></RouterView>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
