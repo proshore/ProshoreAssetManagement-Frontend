d7b7cc1e32e0031e1e7372c923849647de2e735d
saugat977 <jungsaugat@gmail.com>
2022-10-19 14:02:24 +0545

    update: removed console logs

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -2,7 +2,6 @@ export default function getToken() {
     // the data stored in localStorage will change in future, so this function
     //will also require updates
     const token = localStorage.getItem('data')
-    console.log('token',token)
     if (!token){
         return undefined
     }
