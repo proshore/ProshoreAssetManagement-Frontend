97868be5fa617778d4d0ec29c71e188e9bd687eb
saugat977 <jungsaugat@gmail.com>
2022-10-19 14:39:32 +0545

    update:added build folder

--- dist/assets/validateEmail.4d4a0f2a.js
+++
@@ -1,1 +0,0 @@
-const a=s=>{const e={isValid:!0,errorMessage:""};return/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(s)||(e.errorMessage="Please Enter a valid email",e.isValid=!1),e};export{a as v};
