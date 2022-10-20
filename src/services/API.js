fbbb0bf5b2c9980db2553e8a52c861014df4ef3d
saugat977 <jungsaugat@gmail.com>
2022-10-20 15:09:17 +0545

    added withCrediantials on API.js

--- src/services/API.js
+++ src/services/API.js
@@ -6,8 +6,9 @@ const {BACKEND_URI} = backendURI
 
 
 const API = axios.create({
-    baseURL: `${BACKEND_URI}`
+    baseURL: `${BACKEND_URI}`,
+    headers: {'Access-Control-Allow-Origin': '*'}
 })
 
-
+API.defaults.withCredentials = true;
 export default API
